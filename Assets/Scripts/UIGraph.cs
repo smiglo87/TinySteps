1b096014daf81fc238bab9b1808d591e32c36488
GregLukosek <greg@the-app-developers.co.uk>
2013-12-29 16:47:47 +0000

    - graph label prefab added - labels on axis x done

--- Assets/Scripts/UIGraph.cs
+++ Assets/Scripts/UIGraph.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using Vectrosity;
 
 public class UIGraph : MonoBehaviour {
 
+
+	public UserManager userManager;
 
 	public enum Span { all, year, month, week };
 
@@ -15,6 +18,7 @@ public class UIGraph : MonoBehaviour {
 	public VectorLine line;
 	public Material lineMaterial;
 	public Material axisMaterial;
+	public GameObject axisLabelPrefab;
 
 	void Awake()
 	{
@@ -26,14 +30,16 @@ public class UIGraph : MonoBehaviour {
 
 	void Start()
 	{
-		SetCanvas();
+		SetCanvas(Span.all, DateTime.Now);
 	}
 
 
 	/// <summary>
 	/// Setting up both axes and labels
 	/// </summary>
-	void SetCanvas()
+	/// <param name="span">Span of the graph</param>
+	/// <param name="dateMax">Date max is simply max time on x axis</param>
+	void SetCanvas(Span span, DateTime dateMax)
 	{
 		Vector3 originPosition = canvas.transform.position;
 	
@@ -48,6 +54,33 @@ public class UIGraph : MonoBehaviour {
 		VectorLine xAxis = new VectorLine("xAxis", xAxisPoints, axisMaterial, 2f);
 		xAxis.Draw3D();
 
+		//spawn X Labels
+		if (span == Span.all)
+		{
+			for (int i=0; i<2; i++)
+			{
+				GameObject labelGO = (GameObject)Instantiate(axisLabelPrefab);
+				labelGO.transform.parent = transform;
+				labelGO.transform.localScale = Vector3.one;
+				UILabel label = labelGO.GetComponent<UILabel>();
+
+				//first (origin) label
+				if (i==0)
+				{
+					labelGO.transform.localPosition = Vector3.zero;
+					label.text = userManager.babies[userManager.currentBaby].dateOfBirth.ToString("MMM\nyyyy");
+				}
+				//end label
+				else if (i==1)
+				{
+					labelGO.transform.localPosition = new Vector3(canvas.width, 0, 0);
+					label.text = dateMax.ToString("MMM\nyyyy");
+				}
+
+			}
+
+		}
+
 
 
 
@@ -58,7 +91,6 @@ public class UIGraph : MonoBehaviour {
 	void DrawLine(Vector2[] points)
 	{
 		line = new VectorLine("line", points, lineMaterial, 2f);
-
 		line.Draw();
 	}
 
